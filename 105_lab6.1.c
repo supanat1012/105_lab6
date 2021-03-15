8e79d8a079665fbc1d5ec7ef7961c83af043cb6d
supanat <supanat.ap@gmail.com>
2021-03-15 19:01:02 +0700

    ฟ

--- 105_lab6.1.c
+++ 105_lab6.1.c
@@ -1,7 +1,24 @@
 #include<stdio.h>
 int main()
-{
+{   int a,n,i,j;
 
+    scanf("%d",&n);
+
+    char A[1000][1000];
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%s",A[i]);
+    }
+
+
+
+    for(i=0;i<n;i++)
+    {
+
+             printf("%s\n",A[i]);
+
+    }
 
 
 
