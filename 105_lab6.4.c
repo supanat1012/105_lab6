8d1e7e1f878fc25544c558a90c93bb86d3202dd6
supanat <supanat.ap@gmail.com>
2021-03-15 16:55:31 +0700

    Update 105_lab6.4.c

--- 105_lab6.4.c
+++ 105_lab6.4.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 int main()
 {
+    int n,i,a;
 
+    scanf("%d",&n);
+
+    int A[n],B[n],count[n];
+
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d %d",&A[i],&B[i]);
+
+        for(;;)
+    }
 
 
 
