ce0aafc314d98995c91e83c3a9e151c562f1d245
supanat <supanat.ap@gmail.com>
2021-03-15 19:19:05 +0700

    a

--- 105_lab6.1.c
+++ 105_lab6.1.c
@@ -4,20 +4,31 @@ int main()
 
     scanf("%d",&n);
 
-    char A[1000][1000];
+    char A[1000][1000],x[1000];
 
     for(i=0;i<n;i++)
     {
         scanf("%s",A[i]);
     }
 
+    for(i=0;i<n;i++)
+    {
+
+            if(strcmp(A[i],A[i+1]==1))
+            {
+                strcpy(x,A[i+1]);
+                strcpy(A[i+1],A[i]);
+                strcpy(A[i],x);
+            }
+
+    }
+
+
 
 
     for(i=0;i<n;i++)
     {
-
-             printf("%s\n",A[i]);
-
+        printf("%s\n",A[i]);
     }
 
 
